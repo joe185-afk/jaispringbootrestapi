8d303f802276b50ed04f071e75c5dac3fadf8373
joeandreas <joeindahsi@gmail.com>
2023-11-23 16:51:41 +0700

    Initial commit

---
+++ src/main/java/com/juaracoding/jaispringbootrestapi/JuaraCodingApplication.java
@@ -0,0 +1,13 @@
+package com.juaracoding.jaispringbootrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuaraCodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuaraCodingApplication.class, args);
+	}
+
+}
